d8cb289585cbbe401c34199fd69b4f240eb1a89c
Tariq-Monowar <101199109+Tariq-Monowar@users.noreply.github.com>
2023-03-18 13:39:06 +0600

    Update user.route.js

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -3,16 +3,15 @@ const fs = require("fs");
 const multer = require("multer");
 const session = require('express-session');
 const {
-   getAlluser,
-   getOneUser, 
+   getAlluser, 
    createUser, 
    deleteUser, 
    updateUser,
    loginUser,
    userProfile,
-   postItem
   } = require('../controller/user.controllers')
 
+  
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, "uploads");
@@ -26,35 +25,23 @@ const upload = multer({ storage: storage });
 
 
 route.use(session({
-  secret: 'my-secret-key', // Set a secret key for the session
+  secret: 'my-secret-key', 
   resave: false,
   saveUninitialized: true
 }));
 
 
-// route.get('/old-route', (req, res) => {
-//   const data = {
-//     userName: "Seam",
-//     id: "435"
-//   };
-//   req.session.data = data;
-//   res.redirect('/api/new-route');
-// });
-
-
-// route.get('/new-route', (req, res) => {
-//   const data = req.session.data;
-//   req.session.data = null;
-//   res.status(200).json(data);
-// });
-
 route.get('/profile',userProfile)
 route.post('/login',loginUser)
-route.post('/items',postItem)
-route.route('/').get(getAlluser).post(upload.single("image"),createUser)
-route.route('/:id').get(getOneUser).patch(upload.single("image"),updateUser).delete(deleteUser)
 
+route.route('/')
+     .get(getAlluser)
+     .post(upload.single("image"),createUser)
 
+route.route('/:id')
+     .patch(upload.single("image"),updateUser)
+     .delete(deleteUser)
 
 
 module.exports = route
+
